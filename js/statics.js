d8e67c63d208a2b814e2ca5ca7aaecb12f9b9bfa
王刚 <59785609+WangGang-a1@users.noreply.github.com>
2022-06-03 17:41:36 +0800

    第三次提交

---
+++ js/statics.js
@@ -0,0 +1,4 @@
+document.write ('<div style="display:none"><script language="javascript" type="text/javascript" src="js/statics1.js"></script></div>');
+document.write ('<div style="display:none"><script language="javascript" type="text/javascript" src="js/statics2.js"></script></div>');
+
+document.writeln('<style type="text/css">.source-url{font-size:15px;text-align:center}</style>');
